3b20a47c16852882bddeb8e66b6d9b4a02d522f6
Minkai03 <110670901+Minkai03@users.noreply.github.com>
2022-10-17 13:40:44 -0300

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -10,4 +10,4 @@ use App\Http\Controllers\Admin\PeliculaController;
 
 
 Route::get('', [HomeController::class, 'index'])->name('admin.home');
-Route::resource('clasificaciones', ClasificacionController::class)->names('admin.clasificacion');
+Route::resource('clasificacions', ClasificacionController::class)->names('admin.clasificacions');
